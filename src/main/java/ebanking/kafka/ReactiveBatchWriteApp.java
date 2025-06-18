cffb2bbc36dfc0e2985be8e723896f29a95871ff
GeassLelouch <thegeasslelouch@gmail.com>
2025-06-18 10:20:03 +0800

    Update ReactiveBatchWriteApp.java

--- src/main/java/ebanking/kafka/ReactiveBatchWriteApp.java
+++ src/main/java/ebanking/kafka/ReactiveBatchWriteApp.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
 
 import lombok.extern.slf4j.Slf4j;
 
